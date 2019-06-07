985a476589903a931dd9e3dd78302fb776f60443
pzakirov <zakirov@cmsplanet.ru>
2019-06-07 16:23:34 +0300

    navbar without useStyles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,26 +2,51 @@ import AppBar from '@material-ui/core/AppBar'
 import React, { Component } from 'react'
 import UserLinks from './UserLinks'
 import AuthLinks from './AuthLinks';
-import { Toolbar } from '@material-ui/core';
+import { Toolbar, IconButton, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import '../../styles/navbar.css';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
+
+    renderLeftMenu = () => {
+        return (
+            <IconButton
+                edge="start"
+                className='menu-btn'
+                color="inherit"
+                aria-label="Open drawer"
+            >
+                <MenuIcon />
+            </IconButton>
+        )
+    }
+
+    renderLinks = () => {
+        if (true) {
+            return <UserLinks />;
+        }
+        return <AuthLinks />
+    }
+
     render() {
         return (
-            <AppBar className='navbar'>
-                <Toolbar>
-                    <AuthLinks />
-                    <UserLinks />
-                </Toolbar>
-            </AppBar>
+            <div className='grow'>
+                <AppBar className='navbar' position="static">
+                    <Toolbar>
+                        {this.renderLeftMenu()}
+                        <NavLink to="/" className='home-link'>
+                            <Typography variant="h6" className=''>
+                                Home
+                            </Typography>
+                        </NavLink>
+                        <div className='section-desktop'>
+                            {this.renderLinks()}
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
         );
-        return (
-            <>
-                <div>HI</div>
-                <AuthLinks />
-                <UserLinks />
-            </>
-        )
     }
 }
 export default Navbar
--- client/src/components/layout/UserLinks.js
+++ client/src/components/layout/UserLinks.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { IconButton, Badge } from '@material-ui/core';
+import MailIcon from '@material-ui/icons/Mail';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 export default (props) => (
-    <div>
-        <span>avatar</span>
-    </div>
+    <>
+        <IconButton aria-label="Show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+            </Badge>
+        </IconButton>
+        <IconButton
+            edge="end"
+            aria-label="Account of current user"
+            aria-controls='profile'
+            aria-haspopup="true"
+            onClick={console.log}
+            color="inherit"
+        >
+            <AccountCircle />
+        </IconButton>
+    </>
 )
