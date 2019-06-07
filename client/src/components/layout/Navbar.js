26e94891a0423352e25753acddef6f38d7cac4d6
pzakirov <zakirov@cmsplanet.ru>
2019-06-07 15:58:03 +0300

    blue color navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,8 +2,19 @@ import AppBar from '@material-ui/core/AppBar'
 import React, { Component } from 'react'
 import UserLinks from './UserLinks'
 import AuthLinks from './AuthLinks';
+import { Toolbar } from '@material-ui/core';
+import '../../styles/navbar.css';
+
 class Navbar extends Component {
     render() {
+        return (
+            <AppBar className='navbar'>
+                <Toolbar>
+                    <AuthLinks />
+                    <UserLinks />
+                </Toolbar>
+            </AppBar>
+        );
         return (
             <>
                 <div>HI</div>
