9a7320563d8744d8f47e931bfe5c9493cdb43426
Pavel Zakirov <zakirov@cmsplanet.ru>
2019-06-10 21:16:35 +0300

    some styles

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,35 +1,31 @@
-
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from '../auth/Login';
-import Home from '../home/Home';
-import Profile from '../profile/Profile';
-import Register from '../auth/Register';
-import Navbar from './Navbar';
-import LeftMenu from './leftMenu/LeftMenu';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "../auth/Login";
+import Home from "../home/Home";
+import Profile from "../profile/Profile";
+import Register from "../auth/Register";
+import Navbar from "./Navbar";
+import LeftMenu from "./leftMenu/LeftMenu";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class Layout extends Component {
-	render() {
-		const { isAuthenticated } = this.props;
-		return (
-			<BrowserRouter>
-				<div className="App">
-					<Navbar />
-					<div>
-						{isAuthenticated && <LeftMenu isAuthenticated={isAuthenticated} />}
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/register" component={Register} />
-							<Route exact path="/profile" component={Profile} />
-						</Switch>
-					</div>
-				</div>
-			</BrowserRouter>
-		);
-	}
+    render() {
+        const { isAuthenticated } = this.props;
+        return (
+            <BrowserRouter>
+                <Navbar />
+                <section className='layout'>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/profile" component={Profile} />
+                    </Switch>
+                </section>
+            </BrowserRouter>
+        );
+    }
 }
 
 Layout.propTypes = {
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,33 +1,43 @@
-import React, { Component } from 'react';
-import AccountLinks from './AccountLinks';
-import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import AccountLinks from "./AccountLinks";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import logo from "../../images/logo.png";
 
 class Navbar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			menuIsOpened: false
-		};
-	}
-
-	render() {
-		const { isAuthenticated } = this.props;
-		return (
-			<div>
-				NavBar
-			</div>
-		);
-	}
+    render() {
+        const { isAuthenticated } = this.props;
+        return (
+            <header>
+                <nav>
+                    <div className="hamburger">
+                        <div className='line' />
+                        <div className='line' />
+                        <div className='line' />
+                    </div>
+                    <ul className="nav-links">
+                        <li>
+                            <NavLink to="/" className='logo'>
+                                <img src={logo} />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <a href="#">Профиль</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
 }
 
 Navbar.propTypes = {
-	isAuthenticated: PropTypes.bool
+    isAuthenticated: PropTypes.bool
 };
 
-const mapStateToProps = (state) => ({
-	isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps)(Navbar);
