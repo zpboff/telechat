b099ba5d8ea67bd7f16cc47b4b975b592e0fa588
Pavel <zpb93@yandex.ru>
2019-06-15 18:15:40 +0300

    react-cropper //maybe remove it in the next  iter

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import withAuth from '../shared/withAuth';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Cropper from 'cropperjs'
+import 'cropperjs/dist/cropper.css';
+import Cropper from 'react-cropper';
 
 class Profile extends Component {
 	constructor(props) {
@@ -14,9 +15,22 @@ class Profile extends Component {
 		};
 	}
 
+	_crop() {
+		const dataUrl = this.refs.cropper.getCroppedCanvas().toDataURL();
+		this.setState({ avatarPreview: dataUrl });
+	}
+
 	render() {
 		return (
-			<div className="profile-page">
+			<div>
+				<Cropper
+					ref="cropper"
+					src="https://cdn-images-1.medium.com/max/1600/1*9lrxxiFyoHt0v5GWbYMwZg.png"
+					style={{ height: 400, width: '100%' }}
+					guides={false}
+				/>
+				<button onClick={this._crop.bind(this)}>Crop</button>
+				<img src={this.state.avatarPreview} />
 			</div>
 		);
 	}
