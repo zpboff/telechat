4433b30a09f5ff16c273d4cffd3026851dd26972
pzakirov <zakirov@cmsplanet.ru>
2019-06-20 10:18:21 +0300

    first variant of croppie

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import withAuth from '../shared/withAuth';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Croppie from 'croppie';
+import 'croppie/croppie.css'
 
 class Profile extends Component {
 	constructor(props) {
@@ -11,12 +13,34 @@ class Profile extends Component {
 			avatar: [],
 			avatarPreview: ''
 		};
+		this.avatar = React.createRef();
+	}
+
+	componentDidMount() {
+		const opts = {
+			enableResize: true,
+			viewport: {
+				width: 100,
+				height: 100,
+				type: 'square'
+			}
+		}
+		this.cropper = new Croppie(this.avatar.current, opts);
+	}
+
+	getCroppedImage = () => {
+		const args = { type: 'base64', size: 'viewport', format: 'webp', quality: 1, circle: true }
+		this.cropper.result(args).then(value => {
+			console.log(value);
+		});
 	}
 
 	render() {
 		return (
 			<div>
 				{this.props.birthDate}
+				<img ref={this.avatar} src='https://foliotek.github.io/Croppie/demo/demo-1.jpg' alt='avatar' />
+				<button type='button' onClick={this.getCroppedImage}>Загрузить</button>
 			</div>
 		);
 	}
