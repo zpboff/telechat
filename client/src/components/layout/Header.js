0c7ebf8e9e9c0ce488545bf1e99ce0fbc66e5451
pzakirov <zakirov@cmsplanet.ru>
2019-08-23 11:16:09 +0300

    new logout icon and new logo

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
-import SigninLinks from "./SigninLinks";
-import SignoutLinks from "./SignoutLinks";
-import { inject, observer } from "mobx-react";
-import { NavLink } from "react-router-dom";
+import React, { Component } from 'react';
+import SigninLinks from './SigninLinks';
+import SignoutLinks from './SignoutLinks';
+import { inject, observer } from 'mobx-react';
+import { NavLink } from 'react-router-dom';
 
-@inject("auth")
+@inject('auth')
 @observer
 class Header extends Component {
-    getLinks = () => {
-        if (this.props.auth.isAuthenticated) {
-            return <SignoutLinks />;
-        }
-        return <SigninLinks />;
-    };
+	getLinks = () => {
+		if (this.props.auth.isAuthenticated) {
+			return <SignoutLinks />;
+		}
+		return <SigninLinks />;
+	};
 
-    render() {
-        return (
-            <header>
-                <section className="logo-container">
-                    <NavLink to="/" activeClassName="active" className='icon'>
-                        <i class="material-icons" title="Главная">
-                            home
-                        </i>
-                    </NavLink>
-                </section>
-                <nav>
-                    <ul>{this.getLinks()}</ul>
-                </nav>
-            </header>
-        );
-    }
+	render() {
+		return (
+			<header>
+				<section className="logo-container">
+					<NavLink to="/" activeClassName="active" className="icon">
+						t
+					</NavLink>
+					<div className="logo-filler" />
+				</section>
+				<nav>
+					<ul>{this.getLinks()}</ul>
+				</nav>
+			</header>
+		);
+	}
 }
 export default Header;
--- client/src/components/layout/SignoutLinks.js
+++ client/src/components/layout/SignoutLinks.js
@@ -1,26 +1,21 @@
-import React, { Component, Fragment } from "react";
-import { inject, observer } from "mobx-react";
-import { NavLink } from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import { inject, observer } from 'mobx-react';
+import { NavLink } from 'react-router-dom';
 
-@inject("auth")
+@inject('auth')
 @observer
 class SignoutLinks extends Component {
-    render() {
-        return (
-            <Fragment>
-                <li>
-                    <NavLink
-                        to="/logout"
-                        activeClassName="active"
-                        title="Выход"
-                        className="icon"
-                    >
-                        <i class="material-icons">input</i>
-                    </NavLink>
-                </li>
-            </Fragment>
-        );
-    }
+	render() {
+		return (
+			<Fragment>
+				<li>
+					<NavLink to="/logout" activeClassName="active" title="Выход" className="icon">
+						<i class="material-icons">exit_to_app</i>
+					</NavLink>
+				</li>
+			</Fragment>
+		);
+	}
 }
 
 export default SignoutLinks;
