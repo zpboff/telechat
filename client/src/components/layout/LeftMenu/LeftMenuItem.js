bbf2538016768ead400cff8ab634f8504c4235de
pzakirov <zakirov@cmsplanet.ru>
2019-09-09 13:57:50 +0300

    shared icon component

--- client/src/components/layout/LeftMenu/LeftMenuItem.js
+++ client/src/components/layout/LeftMenu/LeftMenuItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Icon from '../../shared/Icon';
 
 export default function LeftMenuItem({ link, title, icon }) {
 	return (
 		<li>
 			<NavLink to={link} className="icon" activeClassName="active" title={title}>
-				<i class="material-icons">{icon}</i>
+				<Icon icon={icon} />
 			</NavLink>
 		</li>
 	);
---
+++ client/src/components/shared/Icon.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Icon({ icon }) {
+	return <i class="material-icons">{icon}</i>;
+}
