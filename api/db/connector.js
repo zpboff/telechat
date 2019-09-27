06ff4aeab31d3e2dd0fe8e4344584c3d52c6405c
pzakirov <zakirov@cmsplanet.ru>
2019-09-27 15:10:51 +0300

    some new param

--- api/db/connector.js
+++ api/db/connector.js
@@ -2,17 +2,15 @@ const ConnectionStrings = require('../constants/conStrings');
 const mongoose = require('mongoose');
 
 const initializeDbConnection = () => {
-   mongoose.connect(
-      ConnectionStrings.DbRoute,
-      {
-         useNewUrlParser: true,
-         useCreateIndex: true
-      }
-   );
+	mongoose.connect(ConnectionStrings.DbRoute, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	});
 
-   let db = mongoose.connection;
-   db.once("open", () => console.log("connected to the database"));
-   db.on("error", console.error.bind(console, "MongoDB connection error:"));
-}
+	let db = mongoose.connection;
+	db.once('open', () => console.log('connected to the database'));
+	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+};
 
- module.exports = initializeDbConnection
+module.exports = initializeDbConnection;
