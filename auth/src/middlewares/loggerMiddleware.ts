1ec41f9ede6f72ec596f72a7856e6c81be861a14
p.zakirov <zakirov@cmsplanet.ru>
2021-09-06 15:35:25 +0300

    expand log object

--- auth/src/middlewares/loggerMiddleware.ts
+++ auth/src/middlewares/loggerMiddleware.ts
@@ -3,28 +3,49 @@ import { method } from "lodash";
 import { logger } from "../services";
 
 type LogEntity = {
+    timestamp: Date;
     method: string;
     url: string;
+    baseUrl: string;
+    originalUrl: string;
     rawHeaders: string[];
     cookies: any;
     ip: string;
     query: any;
     body: any;
+    remoteAddress: string | undefined;
+    remoteFamily: string | undefined;
+    remotePort: number | undefined;
+    processingTime: number;
 }
 
-export async function loggerMiddleware(req: Request, _res: Response, next: NextFunction) {
-    const { method, url, rawHeaders, cookies, ip, query, body } = req;
-    const logEntity: LogEntity = {
-        method,
-        body, 
-        cookies,
-        ip,
-        query,
-        rawHeaders,
-        url
-    }
+export async function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
+    const requestStart = Date.now();
 
-    logger.info(logEntity, `Request {url}`);
+    res.on('finish', () => {
+        const { method, url, rawHeaders, cookies, ip, query, body, socket, baseUrl, originalUrl } = req;
+        const { remoteFamily, remoteAddress, remotePort } = socket;
+    
+        const logEntity: LogEntity = {
+            timestamp: new Date(),
+            processingTime: Date.now() - requestStart,
+            method,
+            body, 
+            cookies,
+            ip,
+            query,
+            rawHeaders,
+            url,
+            remoteAddress,
+            remoteFamily, 
+            remotePort,
+            baseUrl,
+            originalUrl
+        }
+    
+        logger.info(logEntity, `Request {url}`);
+    })
+    
     
     next();
 }
